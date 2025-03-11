b0a5fc0384318bd9fd8dadc7980593905c3b3eda
MouadHallaffou <mouadhallaffou@gmail.com>
2025-03-11 22:56:55 +0000

    Ajout des seeders pour Categories et Tags

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -11,16 +11,6 @@ class CategorySeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    // public function run(): void
-    // {
-    //     $randArray = [null, 1,2,3,4,5,6,7,8,9,10];
-
-    //     Category::factory(50)->create()->each(function ($categories) use ($randArray){
-    //         $category->parent_id = $randArray[rand(0, count($randArray) - 1)];
-    //         $category->save();
-    //     });
-    // }
-    
     public function run(): void
     {
         $mainCategories = Category::factory(10)->create();
