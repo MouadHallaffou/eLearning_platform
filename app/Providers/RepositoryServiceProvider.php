b3920dfe5bca7f2b0c544bbfca91093224578b74
MouadHallaffou <mouadhallaffou@gmail.com>
2025-03-11 22:57:23 +0000

    Ajout des Providers pour linjection des repositories

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\TagRepository;
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\CategoryRepository;
+use App\Interfaces\TagRepositoryInterface;
+use App\Interfaces\CategoryRepositoryInterface;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(TagRepositoryInterface::class, TagRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
