a9b8a3d733951fa8b3cbc79672ffadc33abdee56
MouadHallaffou <mouadhallaffou@gmail.com>
2025-03-11 22:57:52 +0000

    Ajout des resources API pour la transformation des données

---
+++ app/Http/Resources/TagResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+            'update_at' => $this->update_at,
+        ];
+    }
+}
