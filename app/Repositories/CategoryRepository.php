086982dcef69f106821782c4a45a3f38752e0cc7
MouadHallaffou <mouadhallaffou@gmail.com>
2025-03-13 02:12:37 +0000

    Refactor CategoryRepository for better data handling

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -7,33 +7,36 @@ use App\Models\Category;
 
 class CategoryRepository implements CategoryRepositoryInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+   /**
+    * Create a new class instance.
+    */
+   public function __construct()
+   {
+      //
+   }
 
-    public function index(){
-        return Category::all();
-    }
+   public function index()
+   {
+      return  Category::with('children')->whereNull('parent_id')->get();
+   }
 
-    public function getById($id){
-       return Category::findOrFail($id);
-    }
+   public function getById($id)
+   {
+      return Category::findOrFail($id);
+   }
 
-    public function store(array $data){
-       return Category::create($data);
-    }
+   public function store(array $data)
+   {
+      return Category::create($data);
+   }
 
-    public function update(array $data,$id){
-       return Category::whereId($id)->update($data);
-    }
-    
-    public function delete($id){
-       Category::destroy($id);
-    }
+   public function update(array $data, $id)
+   {
+      return Category::whereId($id)->update($data);
+   }
 
+   public function delete($id)
+   {
+      Category::destroy($id);
+   }
 }
-
