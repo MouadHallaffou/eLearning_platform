a70f28190cc57b10329612fcae545d5a15d79d65
MouadHallaffou <mouadhallaffou@gmail.com>
2025-03-13 02:13:04 +0000

    Update DatabaseSeeder to call new seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            // CategorySeeder::class,
-            // TagSeeder::class,
+            CategorySeeder::class,
+            TagSeeder::class,
+            CourseSeeder::class,
         ]);
     }
 }
