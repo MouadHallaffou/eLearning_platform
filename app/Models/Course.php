e8963e492d7d2ca579b5096ad556cecd56282fa7
MouadHallaffou <mouadhallaffou@gmail.com>
2025-03-19 02:08:27 +0000

    Ajout du modèle, de la factory et du seeder pour les cours

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -34,4 +34,9 @@ class Course extends Model
     {
         return $this->belongsToMany(Tag::class, 'course_tag');
     }
+
+    public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class);
+    }
 }
